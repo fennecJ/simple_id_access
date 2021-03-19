5e1afd6eb05bccd64c6b3d733f9e243d41ba845c
fennecJ <f44071055@gs.ncku.edu.tw>
2021-03-19 23:09:17 +0800

    Now can search student via id

--- dist/try.js
+++ dist/try.js
@@ -13,11 +13,9 @@ var jsonf = (function () {
 })();
 
 
-
 $(document).ready(function() {
   $('#ajax-form button[type="submit"]').click((event) => {
     event.preventDefault()
-
 $.get('./step5', {
   sid: $('#ajax-form input[name=sid]').val(),
   sname:jsonf[$('#ajax-form input[name=sid]').val()]
@@ -27,32 +25,6 @@ $.get('./step5', {
 })
     // Step 11 code goes here
 })
-
-
-/*
-$('#sdata button[type="submit"]').click((event)=>{
-    event.preventDefault()
-var objs = {
-table=[]
-};
-var sid =  $('#sdata input[name=sid]').val();
-var sname = $('#sdata input[name=sname]').val();
-obj.table[sid]=sname;
-var jsond = JSON.stringify(obj);
-
-fs.readFile('students.json', 'utf8', function readFileCallback(err, data){
-    if (err){
-        console.log(err);
-    } else {
-    var obj = JSON.parse(data); //now it an object
-    obj.table[sid]=sname;
-    json = JSON.stringify(obj); //convert it back to json
-    fs.writeFile('students.json', json, 'utf8', callback); // write it back
-}});
-});
-*/
-
-
 
 });
 
@@ -65,3 +37,14 @@ $.post('/gsi',{},function(data){
     $('#ajax-output').html(j);
 });
 });
+$(document).ready(function() {
+$('#FSD').click( event =>{
+    event.preventDefault()
+    $.get('/fsd',{
+    sid: $('#fsdata input[name=sid]').val()
+}, (data)=>{
+        $('#ajax-output').html(data);
+})
+})
+
+})
--- server.js
+++ server.js
@@ -4,21 +4,48 @@
  const bodyParser = require('body-parser')
  var fs = require('fs')
  app.use(bodyParser.urlencoded({ extended: false }))
-
-
- app.post('/gsi',(req,res)=>{
- 
-    fs.readFile('students.json', (err, data) => {
-        if (err) throw err;
-        let student = JSON.parse(data);
-        res.send(student);
-    });
-
- }
- )
  app.use(express.static(`${__dirname}/dist`))
  app.listen(port, () => {
  console.log(`listening on port: ${port}`)
  })
 
+
+
+ app.post('/gsi',(req,res)=>{
+    fs.readFile('students.json', (err, data) => {
+        if (err) throw err;
+        let student = JSON.parse(data);
+        res.send(student);
+    });
+ }
+)
+
+
+
+app.get('/fsd',(req,res)=>{
+    var t =`${req.query.sid}`;
+    console.log(t);
+    fs.readFile('students.json', (err, data) => {
+        if (err) throw err;
+        let tmp=JSON.parse(data);
+        res.send(`Hello, `+tmp[t]);
+    });
+    
+ }
+)
+
+/*
+ app.post('/fsd',(req,res)=>{
+    fs.readFile('students.json', (err, data) => {
+        if (err) throw err;
+        let student = JSON.parse(data);
+        var t =`${req.query.sname}`;
+        let tar = student[t];
+        res.send(`Hello, `+t);
+]);
+    });
+}
+
+*/
+ 
 
