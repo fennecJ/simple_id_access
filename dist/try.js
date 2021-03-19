03ed7b2cc87abc307c36de56f6405eb6b21b91dc
fennecJ <f44071055@gs.ncku.edu.tw>
2021-03-20 01:01:36 +0800

    Prettify

--- dist/try.js
+++ dist/try.js
@@ -1,32 +1,5 @@
-var jsonf = (function () {
-    var jsonf = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': 'students.json',
-        'dataType': "json",
-        'success': function (data) {
-            jsonf = data;
-        }
-    });
-    return jsonf;
-})();
 
 
-$(document).ready(function() {
-  $('#ajax-form button[type="submit"]').click((event) => {
-    event.preventDefault()
-$.get('./step5', {
-  sid: $('#ajax-form input[name=sid]').val(),
-  sname:jsonf[$('#ajax-form input[name=sid]').val()]
-}, (data) => {
-    //   document.getElementById('ajax-output').innerHTML = data;
-    $('#ajax-output').html(data);
-})
-    // Step 11 code goes here
-})
-
-});
 $(document).ready(function() {
 
 $('#GSD').click(function(){
