3b498d3871143d65399c0ed0376ae87bf59434b8
fennecJ <f44071055@gs.ncku.edu.tw>
2021-03-22 17:40:57 +0800

    Prohibit illegal input

--- dist/try.js
+++ dist/try.js
@@ -18,7 +18,9 @@ $('#FSD').click( event =>{
     var a = $('#fsdata input[name=sid]').val().toString().replaceAll(/\s/g,'');
     if((a==''))
     alert('表單資料不能為空');
-
+    else if(containSpecial(a)){
+        alert('含有不合法的字元\n不合法的字元包含(\ )(\~)(\!)(\@)(\#)(\$)(\%)(\^)(\&)(\*)(\()(\))(\-)(\_)(\+)(\=)(\[)(\])(\{)(\})(\|)(\\)(\;)(\:)(\')(\")(\,)(\.)(\/)(\<)(\>)(\?)(\)');
+    }
 else{
     $.get('/fsd',{
     sid: $('#fsdata input[name=sid]').val()
@@ -35,6 +37,9 @@ $('#ASD').click( event =>{
     var b = $('#sdata input[name=sname]').val().toString().replaceAll(/\s/g,'');
     if((a=='')||(b==""))
     alert('表單資料不能為空');
+    else if(containSpecial(a)||containSpecial(b)){
+        alert('含有不合法的字元\n不合法的字元包含(\ )(\~)(\!)(\@)(\#)(\$)(\%)(\^)(\&)(\*)(\()(\))(\-)(\_)(\+)(\=)(\[)(\])(\{)(\})(\|)(\\)(\;)(\:)(\')(\")(\,)(\.)(\/)(\<)(\>)(\?)(\)');
+    }
 else{
     $.get('/asd',{
     sid: $('#sdata input[name=sid]').val(),
@@ -56,8 +61,10 @@ $('#DSD').click( event =>{
     var a = $('#dsdata input[name=sid]').val().toString().replaceAll(/\s/g,'');
     if((a==''))
     alert('表單資料不能為空');
-
-else{
+    else if(containSpecial(a)){
+        alert('含有不合法的字元\n不合法的字元包含(\ )(\~)(\!)(\@)(\#)(\$)(\%)(\^)(\&)(\*)(\()(\))(\-)(\_)(\+)(\=)(\[)(\])(\{)(\})(\|)(\\)(\;)(\:)(\')(\")(\,)(\.)(\/)(\<)(\>)(\?)(\)');
+    }
+    else{
     $.get('/dsd',{
     sid: $('#dsdata input[name=sid]').val()
 },(data)=>{
@@ -73,3 +80,10 @@ else{
 
 
 })
+
+
+function containSpecial( s )      
+{      
+  var containSpecial = RegExp(/[(\ )(\~)(\!)(\@)(\#)(\$)(\%)(\^)(\&)(\*)(\()(\))(\-)(\_)(\+)(\=)(\[)(\])(\{)(\})(\|)(\\)(\;)(\:)(\')(\")(\,)(\.)(\/)(\<)(\>)(\?)(\)]+/);      
+ return ( containSpecial.test(s) );
+}
